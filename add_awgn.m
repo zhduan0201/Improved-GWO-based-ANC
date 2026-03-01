22d01a38b26d7513a606fe27d242a3b0019d768c
zhduan0201 <zhduan_seu@163.com>
2026-03-01 20:44:43 +0800

    Add files via upload

--- add_awgn.m
+++ add_awgn.m
@@ -1,8 +1,13 @@
-function [y,noise] = add_awgn(x,snr) %x: original signal, y: noisy signal, snr: signal-to-noise ratio
-noise=randn(size(x));
-Nx=length(x);
-signal_power=1/Nx*sum(x.*x);
-noise_power=1/Nx*sum(noise.*noise);
-noise_variance=signal_power/(10^(snr/10));
-noise=sqrt(noise_variance/noise_power)*noise;
-y=x+noise;
+function [y, noise] = add_awgn(x, snr)
+% Add Gaussian white noise to the original signal
+% x: original signal 
+% y: noisy signal
+% snr: signal-to-noise ratio
+    noise = randn(size(x));
+    Nx = length(x);
+    signal_power = 1/Nx*sum(x.*x);
+    noise_power = 1/Nx*sum(noise.*noise);
+    noise_variance = signal_power/(10^(snr/10));
+    noise = sqrt(noise_variance/noise_power)*noise;
+    y = x + noise;
+end
